fc20104fbdc01e6667fee957c351c1a9456df433
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-01-23 11:06:55 +1300

    about route/hbs created and has functionality

---
+++ app/routes/about.js
@@ -0,0 +1,30 @@
+import Route from "@ember/routing/route";
+
+let bookmarks = [
+  {
+    id: 1,
+    title: "Microsoft",
+    link: "https://www.microsoft.com/",
+    about: "inrto about Microsoft"
+  },
+  {
+    id: 2,
+    title: "Wikipedia",
+    link: "https://en.wikipedia.org/wiki/Main_Page",
+    about: "The Free Encyclopedia"
+  },
+  {
+    id: 3,
+    title: "Google",
+    link: "https://www.google.com",
+    about: null
+  }
+];
+
+
+
+export default class AboutRoute extends Route {
+    model (){
+        return bookmarks
+    }
+}
